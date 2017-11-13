38937645a2179640b9f9154fcf11cb14c26bfb7d
Julianne Kim <juliannne@gmail.com>
2017-11-13 01:36:38 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application 
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path=="/items/<ITEM NAME>"
+      resp.write "#{item.price}"
+    else 
+      resp.write "Route not found"
+      resp.statue = 400
+    end 
+    resp.finish
+  end
+end
